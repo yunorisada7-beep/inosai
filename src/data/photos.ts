225556fbda679d3b358c97f9d93eb3a612876201
yunorisada7-beep <yunorisada7@gmail.com>
2026-04-23 10:38:42 +0900

    Slideshow: 4枚目に「受験相談会 現役学生がアドバイス」を追加

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -9,6 +9,7 @@ export const photos: PhotoItem[] = [
   { file: "ステージ.jpg", caption: "ステージ" },
   { file: "ステージ企画 亥鼻ダンスサークル.jpg", caption: "ステージ企画 亥鼻ダンスサークル" },
   { file: "医学部企画 心エコー体験.png", caption: "医学部企画 心エコー体験" },
+  { file: "受験相談会 現役学生がアドバイス.png", caption: "受験相談会 現役学生がアドバイス" },
   { file: "大人気！ 縫合体験.png", caption: "大人気！ 縫合体験" },
   { file: "委員会VR・AR企画.jpg", caption: "委員会VR・AR企画" },
   { file: "委員会屋台.jpg", caption: "委員会屋台" },
